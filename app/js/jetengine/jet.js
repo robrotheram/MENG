c79964435951da6c8c6158b588ba9faab28029c3
robert <robrotheram@gmail.com>
2014-12-23 00:20:03 +0000

    New view

--- app/js/jetengine/jet.js
+++ app/js/jetengine/jet.js
@@ -6,28 +6,37 @@ var texture = THREE.ImageUtils.loadTexture('images/metal2.jpg');
 texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
 texture.repeat.set(10, 10);
 var play = false;
-
+var container;
+var $container;
 var _this;
+var raycaster;
+var mouseVector;
 
+function Jet($con) {
 
-function Jet($container) {
-
+  $container = $con;
   _this = this;
-  width = $container.width();
-  height = $container.height();
+  container = document.createElement('div');
+  width = $con.width();
+  height = $con.height();
 
+  container.style.width = width;
+  container.style.height = height;
   renderer = new THREE.WebGLRenderer({alpha: true});
+  renderer.domElement.id = 'jetEngine';
   renderer.setClearColor(0xffffff, 1);
   renderer.setSize(width, height);
-  $container.append(renderer.domElement);
+  container.appendChild(renderer.domElement);
+  $con.append(container);
 
+  raycaster = new THREE.Raycaster();
 
   camera = new THREE.PerspectiveCamera(60, width / height, 1, 2000);
   camera.position.z = 200;
   camera.position.y = 0;
   camera.position.x = 30;
 
-  controls = new THREE.TrackballControls(camera);
+  controls = new THREE.TrackballControls(camera, container);
 
   controls.rotateSpeed = 1.0;
   controls.zoomSpeed = 1.2;
@@ -60,6 +69,8 @@ function Jet($container) {
 
 
   window.addEventListener('resize', _this.onWindowResize, false);
+  window.addEventListener('click', _this.onMouseMove, false);
+
   _this.animate();
 }
 
@@ -76,8 +87,26 @@ Jet.prototype.changeColor = function () {
   }
 };
 
-Jet.prototype.playPause = function () {
+Jet.prototype.playPause = function (btn) {
+
   play = !play;
+  if (play) {
+    $(playbtn).html('Puase');
+  } else {
+    $(playbtn).html('Play');
+  }
+};
+
+Jet.prototype.reset = function () {
+  play = false;
+  for (i = 0; i < jetEngine.length; i++) {
+    jetEngine[i].material = new THREE.MeshNormalMaterial()
+  }
+
+  camera.position.z = 200;
+  camera.position.y = 0;
+  camera.position.x = 30;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 };
 
 
@@ -90,9 +119,9 @@ Jet.prototype.addmodel = function (geometry, materials) {
 };
 
 Jet.prototype.onWindowResize = function () {
-  var $container = $('.webgl');
-  width = $container.width();
-  height = $container.height();
+
+  width = container.width();
+  height = container.height();
   renderer.setSize(width, height);
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
@@ -105,16 +134,52 @@ Jet.prototype.animate = function () {
   if (play) {
 
     if (jetEngine[0] != null) {
-      jetEngine[0].rotateZ(1);
-      jetEngine[1].rotateZ(1);
-      jetEngine[2].rotateZ(10);
-      jetEngine[3].rotateZ(0);
-      jetEngine[4].rotateZ(1);
-      jetEngine[5].rotateZ(30);
+      try {
+        jetEngine[0].rotateZ(1);
+        jetEngine[1].rotateZ(1);
+        jetEngine[2].rotateZ(10);
+        jetEngine[3].rotateZ(0);
+        jetEngine[4].rotateZ(1);
+        jetEngine[5].rotateZ(30);
+      } catch (err) {
+
+      }
+
     }
   }
 
   requestAnimationFrame(_this.animate);
   renderer.render(scene, camera);
+
   controls.update();
+
+
+};
+
+Jet.prototype.onMouseMove = function (e) {
+
+  var isHovered = $container.is(":hover");
+  console.log(isHovered);
+  if (isHovered) {
+    mouseVector = new THREE.Vector3();
+    mouseVector.x = 2 * (e.clientX / width ) - 1;
+    mouseVector.y = 1 - 2 * ( e.clientY / height );
+
+    var vector = mouseVector.clone().unproject(camera);
+    var direction = new THREE.Vector3(0, 0, -1).transformDirection(camera.matrixWorld);
+    raycaster.set(vector, direction);
+    var intersects = raycaster.intersectObjects(jetEngine);
+
+    var intersection = intersects[intersects.length - 1];
+    obj = intersection.object;
+
+    obj.material = new THREE.MeshPhongMaterial({
+      // light
+      map: texture,
+      // dark
+      shininess: 50
+    });
+  }
+
+
 };
