4967bd59fd52a3661c2a7e9a52f3718bcf9b91ed
robert <robrotheram@gmail.com>
2015-02-05 00:50:17 +0000

    New UI V3 Design

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ var debug = require('debug')('node-test');
 var app = require('./app');
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '192.168.0.3';
 
 app.set('port', server_port);
 server = app.listen(app.get('port'), server_ip_address,  function() { debug('Express server listening on port ' + server.address().port);});
 
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+  for (var k2 in interfaces[k]) {
+    var address = interfaces[k][k2];
+    if (address.family === 'IPv4' && !address.internal) {
+      addresses.push(address.address);
+    }
+  }
+}
+
+console.log(addresses);
